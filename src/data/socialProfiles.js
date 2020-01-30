89bf0b374a6ff2a24fefd51722d1a7b75ec71cbe
MH Sunny <sunny@oraiko.com>
2020-01-29 22:46:00 -0500

    initail commit

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+import emailIcon from '../assets/email_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'https://github.com/mhsunny',
+    image: githubIcon
+  },
+  {
+    id: 2,
+    link: 'https://www.linkedin.com/in/mh-sunny/',
+    image: linkedinIcon
+  },
+  // {
+  //   id: 3,
+  //   link: 'https://twitter.com/',
+  //   image: twitterIcon
+  // },
+  {
+    id: 4,
+    link: 'mailto:mhsunny.abcd@gmail.com',
+    image: emailIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
